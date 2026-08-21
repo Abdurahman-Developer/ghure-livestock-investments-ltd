3d5bc9b88dcb5559f0692ca7306d43c8f606144e
abdirahmanmohamud510 <your-abdirahmanmohamud510@gmail.com>
2026-08-21 19:56:39 +0300

    Add login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Login from "./Login";
+import Login from "./login";
 
 const API_BASE_URL =
   import.meta.env?.VITE_API_URL || "http://localhost:3000/api";
