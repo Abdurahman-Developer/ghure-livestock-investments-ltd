08fab1afd01000bc70c799d386ac41002a80f17c
abdirahmanmohamud510 <your-abdirahmanmohamud510@gmail.com>
2026-08-23 08:17:37 +0300

    add helmet CSP headers to express server

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import helmet from "helmet";
 import connectDB from "./src/config/db.js";
 import salesRoutes from "./src/routes/salesRoutes.js";
 import expensesRoutes from "./src/routes/expensesRoutes.js";
 import assetsRoutes from "./src/routes/assetsRoutes.js";
 import debtsRoutes from "./src/routes/debtsRoutes.js";
 import authRoutes from "./src/routes/authRoutes.js";
+
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        fontSrc: ["'self'", "https://fonts.gstatic.com"],
+        styleSrc: ["'self'", "https://fonts.googleapis.com", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:"],
+        scriptSrc: ["'self'"],
+        connectSrc: ["'self'"],
+      },
+    },
+  })
+);
 
 app.use(express.json());
 app.use(
